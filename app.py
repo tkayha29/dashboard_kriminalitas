7962c69e09d882454d302bcae8dccda239fd999b
Alfarabi_mzl <74232912+tkayha29@users.noreply.github.com>
2024-02-28 14:37:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import pandas as pd
 import streamlit as st
 import altair as alt
 
-st.set_page_config(st.set_page_config(
+st.set_page_config(
     page_title="Disparitas Gender Korban Tindakan Kriminal di Los Angeles",
     page_icon=":chart:",
     layout="wide",
     initial_sidebar_state="expanded",                                      
-))                    
+)                    
   
 page_bg_img = f"""
 <style>
